9310fa3a94745b806bb52c5edf0ec4beb42bc12e
Deep <deepguchhait01@gmail.com>
2026-03-07 18:04:03 +0900

    fix the email

--- services/emailService.js
+++ services/emailService.js
@@ -1,8 +1,10 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
-// ───── Transporter ─────
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
