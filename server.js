f1126c5ad80b2175838b8a1cbc5611c7fa79f7d9
Deep <deepguchhait01@gmail.com>
2026-03-14 18:54:28 +0530

    ll

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ app.use(
     credentials: true,
   })
 );
+// Fallback CORS header for all responses
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins[0] || "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
